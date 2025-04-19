df00d5aa1526728e329c12e964ebf0d6c927b494
Ujkan Sulejmani <ujkan99@gmail.com>
2025-04-19 23:28:34 +0200

    improve file/folder structure by extracting fns to other files

---
+++ log_utils.h
@@ -0,0 +1,15 @@
+static inline void print_message(const char *format, ...) {
+  va_list args;
+  va_start(args, format);
+  vprintf(format, args);
+  printf("\n");
+  va_end(args);
+}
+#define PRINT(...) print_message(__VA_ARGS__)
+#ifdef DEBUG
+#define DPRINT(format, ...) printf(format "\n", ##__VA_ARGS__)
+#else
+#define DPRINT(format, ...) ((void)0) /* do nothing */
+#endif
+#define PASS PRINT("Test passed");
+#define FAIL PRINT("Test failed");
