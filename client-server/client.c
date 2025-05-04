76bd27bd2ee8b5ff871b3ccf800fc9cd7be340f5
Ujkan Sulejmani <ujkan99@gmail.com>
2025-05-04 17:25:53 +0200

    more Q&A + change AF_NET to PF_NET

--- client-server/client.c
+++ client-server/client.c
@@ -12,17 +12,34 @@ int main(void) {
 
   struct sockaddr_in addr = {};
   addr.sin_family = AF_INET;
-  addr.sin_port = ntohs(1234);
-  addr.sin_addr.s_addr = ntohl(0x7F000001); // 127.0.0.1
+  // Q: why ntoh vs. hton?
+  // A: it should always be hton, since sockaddr_in expects
+  //    its data in network byte order
+  //    but, hton and ntoh both have same implementation (just
+  //    a byte swap) so code will work fine, just semantics
+  //    are not clear
+  addr.sin_port = htons(1234);
+  // Q: why 127.0.0.1 here vs. 0.0.0.0 in server
+  // A: 0.0.0.0 is not an address per se, it's a wildcard
+  //    (or meta-address). In the bind in the server code
+  //    it's used to tell the system that any address is OK,
+  //    we don't care
+  //    here, we cannot use that. instead, to tell it we're
+  //    looking at stuff in our *local* machine, we specify
+  //    the LOOPBACK address, aka 127.0.0.1, which says
+  //    the address of this machine
+  addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK); // 127.0.0.1
   int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
   if (rv) {
     die("connect()");
   }
 
   char wbuf[] = "hello";
+  // Q: diff between strlen(wbuf) and sizeof(wbuf)?
+  // A: strlen rets 5 (len of 'hello') but sizeof(wbuf) rets 6 (includes '\0')
   write(fd, &wbuf, strlen(wbuf));
 
   char rbuf[64] = {};
-  read(fd, &rbuf, sizeof(rbuf) - 1);
+  read(fd, &rbuf, sizeof(rbuf) - 1); // same as strlen; last byte is '\0'
   printf("Server says: %s\n", rbuf);
 }
--- client-server/server.c
+++ client-server/server.c
@@ -14,12 +14,16 @@ int main(void) {
   // SOCK_STREAM is TCP
   // SOCK_DGRAM is UDP
   // 0 is IP
-  int fd = socket(AF_INET, SOCK_STREAM, 0);
+  int fd = socket(PF_INET, SOCK_STREAM, 0);
   int val = 1;
   setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
   struct sockaddr_in addr = {};
   addr.sin_family = AF_INET;
   addr.sin_port = htons(1234);
+  // Q: why 0.0.0.0 here?
+  // A: 0.0.0.0 serves as '*' wildcard, to suggest
+  //    any address is OK. addr will be chosen by
+  //    TCP/IP subsystem
   addr.sin_addr.s_addr = htonl(0x00000000); // 0.0.0.0
   int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
   if (rv) {
