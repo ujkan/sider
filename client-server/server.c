bcf632e50aa4db1bf0c7fee1d03a9f5face355a6
Ujkan Sulejmani <ujkan99@gmail.com>
2025-06-09 11:50:57 +0200

    event loop v1, no handling of read/write

--- client-server/server.c
+++ client-server/server.c
@@ -1,7 +1,16 @@
+#include "../vector.h"
 #include "utils.h"
+#include <errno.h>
+#include <fcntl.h>
 #include <netinet/in.h>
+#include <poll.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/fcntl.h>
+#include <sys/poll.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -11,6 +20,9 @@ int32_t one_request(int fd) {
   int32_t rv = read_full(fd, len_rbuf, 4);
   if (rv != 0) {
     msg("one_request, read_full len");
+    if (errno == EBADF) {
+      msg("BAD FD");
+    }
     printf("rv:%d\n", rv);
     return rv;
   }
@@ -41,6 +53,41 @@ int32_t one_request(int fd) {
   return write_full(fd, wbuf, 4 + reply_len);
 }
 
+static void fd_set_nonblock(int fd) {
+  int flags = fcntl(fd, F_GETFL);
+  if (flags == -1) {
+    msg("fd_set_nonblock, F_GETFL");
+  }
+  flags |= O_NONBLOCK;
+  fcntl(fd, F_SETFL, flags);
+}
+
+typedef struct conn {
+  int fd;
+  bool want_read;
+  bool want_write;
+
+  char *wbuf;
+  char *rbuf;
+
+} conn;
+
+typedef struct pollfd pollfd_t;
+
+#define COMPARE_CONN(a, b) ((a.fd) == (b.fd) ? 0 : ((a.fd) > (b.fd) ? 1 : -1))
+DEFINE_VECTOR(conn, COMPARE_CONN);
+DEFINE_VECTOR(pollfd_t, COMPARE_CONN);
+
+void conn_init(conn *c, int fd) {
+  c->fd = fd;
+  c->want_read = true;
+  c->want_write = false;
+  c->rbuf = malloc(k_max_msg);
+  c->wbuf = malloc(k_max_msg);
+}
+
+void handle_read(conn *c) { printf("dummy read"); }
+void handle_write(conn *c) { printf("dummy write"); }
 int main(void) {
   // args:
   // domain, type (TCP/UDP), protocol
@@ -67,21 +114,76 @@ int main(void) {
     die("listen()");
   }
 
-  while (1) {
-    struct sockaddr_in client_addr = {};
-    socklen_t addrlen = sizeof(client_addr);
+  printf("FD : %d\n", fd);
 
-    int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
-    if (connfd == -1) {
-      continue; // error
+  fd_set_nonblock(fd);
+
+  vector_conn conns;
+  vector_pollfd_t poll_args;
+  vector_conn_init(&conns, 10);
+  vector_pollfd_t_init(&poll_args, 10);
+
+  while (1) {
+
+    printf("loopiter\n");
+    poll_args.size = 0;
+    pollfd_t pfd = {fd, POLLIN, 0};
+    vector_pollfd_t_push(&poll_args, pfd);
+    msg("pushed listening fd");
+
+    for (int i = 0; i < conns.size; i++) {
+      pollfd_t pfd = {fd, POLLERR, 0};
+      conn *c = vector_conn_get(&conns, i);
+      if (c->want_read) {
+        msg("wanna read");
+        pfd.events |= POLLIN;
+      }
+      if (c->want_write) {
+        pfd.events |= POLLOUT;
+      }
+      vector_pollfd_t_push(&poll_args, pfd);
+    }
+    msg("pushed all connfds");
+    printf("poll_args size:%d\n", poll_args.size);
+    // printf("poll_args data pointer: %p\n", poll_args.data);
+    int dx = vector_pollfd_t_get(&poll_args, 0)->fd;
+    printf("poll_args first fd%d:\n", dx);
+
+    int rv = poll(poll_args.data, (nfds_t)poll_args.size, -1);
+    printf("done polling\n");
+    if (rv < 0 && errno == EINTR) {
+      continue;
+    }
+    if (rv < 0) {
+      die("poll");
     }
 
-    while (1) {
-      int32_t err = one_request(connfd);
-      if (err) {
-        break;
+    if (vector_pollfd_t_get(&poll_args, 0)->revents & POLLIN) {
+      struct sockaddr_in client_addr = {};
+      socklen_t addrlen = sizeof(client_addr);
+      int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
+      if (connfd != -1) {
+        msg("conn accept");
+        fd_set_nonblock(connfd);
+        conn c;
+        conn_init(&c, connfd);
+        vector_conn_push(&conns, c);
       }
     }
-    close(connfd);
+
+    for (int i = 1; i < poll_args.size; i++) {
+      pollfd_t *poll_arg = vector_pollfd_t_get(&poll_args, i);
+      uint32_t ready = poll_arg->revents;
+      conn *c = vector_conn_get(&conns, poll_arg->fd);
+      if (ready & POLLIN) {
+        handle_read(c);
+      }
+      if (ready & POLLOUT) {
+        handle_write(c);
+      }
+      if (ready & POLLERR) {
+        close(c->fd);
+      }
+    }
   }
 }
