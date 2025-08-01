842fbcaad6a5a142c836c0c7cea1a559ade70a41
Ujkan Sulejmani <u.sulejmani@celonis.com>
2025-08-01 19:14:46 +0200

    changes to header and tests due to refactoring of init

--- utils/bytering.h
+++ utils/bytering.h
@@ -17,7 +17,7 @@ struct ByteRing {
   u32 size;
 };
 
-void byte_ring_init(struct ByteRing *br, u32 cap);
+struct ByteRing *byte_ring_init(u32 cap);
 u32 byte_ring_end(struct ByteRing *br);
 void byte_ring_expand(struct ByteRing *br, u32 new_cap);
 void byte_ring_append_n(struct ByteRing *br, const u8 *buf, u32 len);
