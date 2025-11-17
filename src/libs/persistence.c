5870c19dbc65b2a503dd8d54a846846ec84bf65b
Ujkan Sulejmani <ujkan99@gmail.com>
2025-11-17 16:03:56 +0100

    add compact&merge func

--- src/libs/persistence.c
+++ src/libs/persistence.c
@@ -22,8 +22,14 @@
 //
 //
 
-struct Segment {
+struct DbFile {
   int fd;
+  int flags;
+  char filepath[256];
+} DbFile;
+
+struct Segment {
+  struct DbFile file;
   hashmap_si *offset_map;
 } Segment;
 
@@ -33,9 +39,14 @@ struct Segment {
   ((i) = ((i) + (n) >= (cap) ? (((i) + (n)) - (cap)) : (i) + (n)))
 #define WRAP_INCR(i, cap) WRAP_INCR_N(i, 1, cap)
 
-const uint32_t kKeySize = 2;   // in bytes
-const uint32_t kValueSize = 2; // in bytes
-const uint32_t kEntrySize = 4; // in bytes
+#define WRAP_DECR(i, cap) ((i) = ((i) - (1) >= (0) ? ((i) - (1)) : (cap) - (1)))
+
+#define END_MINUS_ONE(tail, cap) ((tail) == 0 ? ((cap) - 1) : (tail) - 1)
+
+const uint32_t kKeySize = 2;      // in bytes
+const uint32_t kValueSize = 2;    // in bytes
+const uint32_t kEntrySize = 4;    // in bytes
+const uint32_t kNumSegments = 10; // in bytes
 
 struct SegmentList {
   struct Segment segments[MAX_SEGMENTS];
@@ -124,7 +135,7 @@ inline __attribute__((always_inline)) int calc_entry_len(int key_len,
          (kValueSize + value_len + kValueSize) + kEntrySize;
 }
 
-int fastest_append(int fd, const char *data, size_t data_len, int offset) {
+int append_to_file(int fd, const char *data, size_t data_len, int offset) {
   // 2. Write the data
   if (pwrite(fd, data, data_len, offset) == (ssize_t)-1) {
     perror("write failed");
@@ -140,35 +151,44 @@ int fastest_append(int fd, const char *data, size_t data_len, int offset) {
   return 0;
 }
 
-int8_t segment_compact(struct Segment *seg) {
+int8_t segment_compact_mrg(struct SegmentList sl) {
   // this has a few stages
   // 1. read from file
   // 2. do the compaction work
   // 3. write to new file
+  //
+  char *segment_srcs[kNumSegments];
+  char *segment_ptrs[kNumSegments];
 
-  struct stat statbuf;
-  if (fstat(seg->fd, &statbuf) < 0) {
-    printf("fstat error");
-    return -1;
+  int maxdiff = 0;
+  for (int i = 0; i < kNumSegments && sl.segments[i].file.fd != 0; i++) {
+    struct Segment seg = sl.segments[i];
+    struct stat statbuf;
+    if (fstat(seg.file.fd, &statbuf) < 0) {
+      printf("fstat error");
+      return -1;
+    }
+
+    segment_srcs[i] =
+        mmap(0, statbuf.st_size, PROT_READ, MAP_SHARED, seg.file.fd, 0);
+    if (segment_srcs[i] == MAP_FAILED) {
+      printf("mmap error");
+      return -1;
+    }
+    printf("[%d] statbuf.st_size: %ld\n", i, statbuf.st_size);
+
+    segment_ptrs[i] = segment_srcs[i] + statbuf.st_size;
+    printf("ptr[%d]: %p\n", i, segment_ptrs[i]);
   }
-
-  char *src;
-  src = mmap(0, statbuf.st_size, PROT_READ, MAP_SHARED, seg->fd, 0);
-  if (src == MAP_FAILED) {
-    printf("mmap error");
-    return -1;
-  }
-  printf("statbuf.st_size: %ld\n", statbuf.st_size);
-
-  char *ptr = src + statbuf.st_size;
-  printf("ptr: %p\n", ptr);
 
   hashmap_si *map = malloc(sizeof(hashmap_si));
   hashmap_si_init(map, 1);
 
   FILE *outf = fopen("seg.tmp", "w");
+  struct DbFile new_segment_file = {
+      .fd = fileno(outf), .flags = O_RDWR, .filepath = "seg.tmp"};
 
-  struct Segment new_seg = {.fd = fileno(outf), .offset_map = map};
+  struct Segment new_seg = {.file = new_segment_file, .offset_map = NULL};
 
   char batch[1 * 1024 * 1024]; // 1 MiB
   char *batch_tail = batch;
@@ -176,51 +196,67 @@ int8_t segment_compact(struct Segment *seg) {
   int offset = 0;
   int rc;
   int total_bytes_written = 0;
-  while (ptr > src) {
-    LString *key = malloc(sizeof(LString));
-    LString *value = malloc(sizeof(LString));
-    deserialize_kv_pair(&ptr, key, value);
-    printf("%.*s -> ", key->len, key->data);
-    printf("%.*s\n", value->len, value->data);
-    // printf("src: %p\n", src);
-    // printf("ptr: %p\n", ptr);
-    // printf("key_len:%d\n", key.len);
-    // printf("val_len:%d\n", key.len);
-    rc = hashmap_si_insert(map, key, offset);
-    if (rc >= 0) {
-      int entry_len = calc_entry_len(key->len, value->len);
-      // if entry larger than batch, serialize into large enough buf and flush
-      // unlikely; in fact should be avoided TODO: make this impossible
-      if (entry_len > sizeof(batch)) {
-        // first flush batch
-        fastest_append(new_seg.fd, batch, batch_tail - batch,
-                       total_bytes_written);
-        total_bytes_written += (batch_tail - batch);
-        batch_tail = batch;
-
-        char entry_buf[entry_len];
-        serialize_kv_pair(key, value, entry_buf);
-        fastest_append(new_seg.fd, entry_buf, entry_len, total_bytes_written);
-        total_bytes_written += entry_len;
-      } else {
-        // will next entry fit?
-        if ((batch_tail - batch) + entry_len > sizeof(batch)) {
-          // no fit, flush
-          fastest_append(new_seg.fd, batch, batch_tail - batch,
+  maxdiff = 1;
+  while (maxdiff > 0) {
+    printf("maxdiff: %d\n", maxdiff);
+    maxdiff = 0;
+    for (int i = END_MINUS_ONE(sl.tail, 10);
+         i + 1 != sl.head && segment_ptrs[i] - segment_srcs[i] > 0;
+         WRAP_DECR(i, 10)) {
+      printf("i=%d ; sl.head = %d\nptr = %p\nsrc = %p\n", i, sl.head,
+             segment_ptrs[i], segment_srcs[i]);
+      LString *key = malloc(sizeof(LString));
+      LString *value = malloc(sizeof(LString));
+      char *ptr = segment_ptrs[i];
+      deserialize_kv_pair(&ptr, key, value);
+      segment_ptrs[i] = ptr;
+      printf("[%d]%.*s -> ", i, key->len, key->data);
+      printf("[%d]%.*s\n", i, value->len, value->data);
+      // printf("src: %p\n", src);
+      // printf("ptr: %p\n", ptr);
+      // printf("key_len:%d\n", key.len);
+      // printf("val_len:%d\n", key.len);
+      rc = hashmap_si_insert(map, key, offset);
+      if (rc >= 0) {
+        int entry_len = calc_entry_len(key->len, value->len);
+        // if entry larger than batch, serialize into large enough buf and flush
+        // unlikely; in fact should be avoided TODO: make this impossible
+        if (entry_len > sizeof(batch)) {
+          // first flush batch
+          append_to_file(new_seg.file.fd, batch, batch_tail - batch,
                          total_bytes_written);
           total_bytes_written += (batch_tail - batch);
           batch_tail = batch;
+
+          char entry_buf[entry_len];
+          serialize_kv_pair(key, value, entry_buf);
+          append_to_file(new_seg.file.fd, entry_buf, entry_len,
+                         total_bytes_written);
+          total_bytes_written += entry_len;
+        } else {
+          // will next entry fit?
+          if ((batch_tail - batch) + entry_len > sizeof(batch)) {
+            // no fit, flush
+            append_to_file(new_seg.file.fd, batch, batch_tail - batch,
+                           total_bytes_written);
+            total_bytes_written += (batch_tail - batch);
+            batch_tail = batch;
+          }
+          serialize_kv_pair(key, value, batch_tail);
+          batch_tail += entry_len;
         }
-        serialize_kv_pair(key, value, batch_tail);
-        batch_tail += entry_len;
+        offset += entry_len;
       }
-      offset += entry_len;
+      free(value->data);
+      free(value);
+      if (segment_ptrs[i] - segment_srcs[i] > maxdiff) {
+        maxdiff = segment_ptrs[i] - segment_srcs[i];
+      }
     }
-    free(value->data);
-    free(value);
   }
 
-  fastest_append(new_seg.fd, batch, batch_tail - batch, total_bytes_written);
+  append_to_file(new_seg.file.fd, batch, batch_tail - batch,
+                 total_bytes_written);
   hashmap_si_print_entries_compact(map);
 
   // LString key2, value2 = {0};
@@ -232,16 +268,20 @@ int8_t segment_compact(struct Segment *seg) {
   // printf("%.*s -> ", key2.len, key2.data);
   // printf("%.*s -> ", value2.len, value2.data);
 
-  return 0;
-}
+  // TODO: err handling
 
-int8_t segment_list_compact_and_merge(struct SegmentList *seg_list) {
-  if (seg_list->head == SENTINEL) {
-    return -1; // empty
+  for (int i = END_MINUS_ONE(sl.tail, 10); i != sl.head; WRAP_DECR(i, 10)) {
+    struct Segment seg = sl.segments[i];
+    close(seg.file.fd);
+    // unlink(seg.file.filepath);
   }
+  sl.segments[END_MINUS_ONE(sl.tail, 10)].file.fd = fileno(outf);
+  sl.segments[END_MINUS_ONE(sl.tail, 10)].offset_map = map;
+  sl.segments[END_MINUS_ONE(sl.tail, 10)].file = new_segment_file;
 
   return 0;
 }
+
 int main() {
   u8 key_data[2] = "hi";
   LString key = {.len = 2, .data = key_data};
@@ -259,11 +299,20 @@ int main() {
   }
   printf("\n");
 
-  FILE *f = fopen(
-      "/home/usulejmani/development/_personal/build-your-own-redis/my_data.bin",
-      "r+");
-  int fd = fileno(f);
-  struct Segment s = {.fd = fd, .offset_map = NULL};
-
-  segment_compact(&s);
+  struct SegmentList sl = {0};
+  sl.head = 0;
+  for (int i = 1; i <= 3; i++) {
+    char *fp = calloc(128, 1);
+    sprintf(fp,
+            "/home/usulejmani/development/_personal/build-your-own-redis/"
+            "seg_%d.bin",
+            i);
+    FILE *f = fopen(fp, "r+");
+    int fd = fileno(f);
+    struct DbFile dbFile = {.fd = fd, .flags = O_RDWR, .filepath = "fp"};
+    struct Segment s = {.file = dbFile, .offset_map = NULL};
+    sl.segments[i - 1] = s;
+    sl.tail = i - 1;
+  }
+  segment_compact_mrg(sl);
 }
