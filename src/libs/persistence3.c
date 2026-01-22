85f6bfe259f7484c61a494628929af096989d046
Ujkan Sulejmani <ujkan99@gmail.com>
2026-01-22 22:33:30 +0100

    add search in sst logic with index

--- src/libs/persistence3.c
+++ src/libs/persistence3.c
@@ -45,6 +45,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
+#include <sys/stat.h>
 #include <time.h>
 const u32 kTagSize = 2; // bytes
 uint16_t kKeyTag = 0;
@@ -64,18 +66,6 @@ int ceil_div(int a, int b) {
   return 1 + ((a - 1) / b);
 }
 
-struct Store {
-  // serves requests, tier 1
-  SkipList *active_memtable;
-  // these also serve requests, tier 2
-  // TODO: make it a queue
-  // TODO: async thread dumps these to SSTable
-  // once dump is done, pop from here
-  Array *inactive_memtables;
-  // these also serve requests, tier 3
-  Array *sstables;
-};
-
 struct KeyOffsetPair {
   LString *key;
   u32 offset;
@@ -91,7 +81,7 @@ SSTable *dump_memtable_to_sst(SkipList *mt, LString *filepath) {
   free(fp);
 
   if (fptr == NULL) {
-      return NULL;
+    return NULL;
   }
   sst->fd = fileno(fptr);
 
@@ -110,7 +100,11 @@ SSTable *dump_memtable_to_sst(SkipList *mt, LString *filepath) {
   int idx_struct_size = sizeof(struct KeyOffsetPair);
   Array *offset_sparse_index = array_sized_new(out_count, idx_struct_size);
 
-  u32 batch_size = 1 << 6;
+  u32 batch_size = 1 << 3;
+
+  u32 index_size = 0;
+  u32 index_offset = 0;
+  u32 kFooterSize = 4 + 4; // 4 bytes for size; 4 for offset
 
   // --- Write the data and build the index ---
   for (u32 i = 0; i < out_count; i++) {
@@ -130,20 +124,30 @@ SSTable *dump_memtable_to_sst(SkipList *mt, LString *filepath) {
     fwrite(values[i]->data, 1, values[i]->len, fptr);
   }
 
+  index_offset = ftell(fptr);
+
   // --- Write the index ---
   u32 index_count = (out_count - 1) / batch_size + 1;
   for (u32 i = 0; i < index_count; i++) {
     struct KeyOffsetPair *pair =
         &array_index(offset_sparse_index, struct KeyOffsetPair, i);
 
+    fwrite(&kKeyTag, kTagSize, 1, fptr);
+    fwrite(&keys[i]->len, kLStringLenSize, 1, fptr);
     fwrite(pair->key->data, 1, pair->key->len, fptr);
-
     fwrite(&pair->offset, sizeof(u32), 1, fptr);
+    index_size += kTagSize + kLStringLenSize + pair->key->len + sizeof(u32);
   }
+
+  // write footer
+  printf("index_offset: %d\n", index_offset);
+  printf("index_size: %d\n", index_size);
+  fwrite(&index_offset, sizeof(index_offset), 1, fptr);
+  fwrite(&index_size, sizeof(index_size), 1, fptr);
 
   // Return total bytes written
   sst->size = (u32)(ftell(fptr) - start_pos);
-fclose(fptr); // This is mandatory to see the data on disk
+  fclose(fptr); // This is mandatory to see the data on disk
   return sst;
 }
 
@@ -207,6 +211,125 @@ size_t serialize_skip_list(SkipList *mt, char *buf) {
 
   return buf - start;
 }
+void hex_dump(const char *desc, const void *addr, int len) {
+  int i;
+  unsigned char buff[17];
+  const unsigned char *pc = (const unsigned char *)addr;
+
+  // Output description if provided
+  if (desc != NULL)
+    printf("%s:\n", desc);
+
+  for (i = 0; i < len; i++) {
+    // Print offset at the start of every line
+    if ((i % 16) == 0) {
+      if (i != 0)
+        printf("  %s\n", buff);
+      printf("  %04x ", i);
+    }
+
+    // Print hex value
+    printf(" %02x", pc[i]);
+
+    // Store printable character for the right side
+    if ((pc[i] < 0x20) || (pc[i] > 0x7e)) {
+      buff[i % 16] = '.';
+    } else {
+      buff[i % 16] = pc[i];
+    }
+    buff[(i % 16) + 1] = '\0';
+  }
+
+  // Pad out last line if not exactly 16 characters
+  while ((i % 16) != 0) {
+    printf("   ");
+    i++;
+  }
+
+  // Final print of the ASCII buffer
+  printf("  %s\n", buff);
+}
+
+void deserialize_index(char *index, int len, Array *keys_out,
+                       Array *offsets_out) {
+  char buf[8];
+  char *curr = index;
+  while ((curr - index) < len) {
+    curr += kTagSize;
+    LString key = {0};
+
+    memcpy(&(key.len), curr, kLStringLenSize);
+    curr += kLStringLenSize;
+    key.data = malloc(key.len);
+    memcpy(key.data, curr, key.len);
+    curr += key.len;
+    array_push(keys_out, &key);
+
+    u32 offset;
+    memcpy(&offset, curr, sizeof(offset));
+    curr += sizeof(offset);
+    array_push(offsets_out, &offset);
+  }
+}
+
+LString *search_in_sst(SSTable sst, LString *key) {
+  char *fp;
+  fp = lstring_to_cstr(
+      sst.filepath); // 0-terminated means can copy 127 chars max
+  FILE *fptr = fopen(fp, "r");
+
+  struct stat st;
+  fstat(fileno(fptr), &st);
+
+  fseek(fptr, st.st_size - 8, SEEK_SET);
+  u32 index_offset;
+  u32 index_size;
+  fread(&index_offset, 1, sizeof(index_offset), fptr);
+  fread(&index_size, 1, sizeof(index_size), fptr);
+  fseek(fptr, index_offset, SEEK_SET);
+
+  char *index = malloc(index_size);
+  fread(index, index_size, 1, fptr);
+
+  hex_dump("index", index, index_size);
+  Array *keys = array_sized_new(32, sizeof(LString));
+  Array *offsets = array_sized_new(32, sizeof(u32));
+
+  deserialize_index(index, index_size, keys, offsets);
+
+  int low = 0;
+  int high = keys->len - 1;
+  int mid;
+  int offset = -1;
+  while (low <= high) {
+    mid = (high + low) / 2;
+    int cmp_val = lstring_compare(key, &array_index(keys, LString, mid));
+    if (cmp_val == 0) {
+      offset = array_index(offsets, u32, mid);
+      break;
+    } else if (cmp_val > 0) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  if (offset != -1) {
+    fseek(fptr, offset, SEEK_SET);
+    char buf[8];
+    fread(buf, 1, kTagSize + kLStringLenSize, fptr);
+    if (memcmp(buf, &kPairTag, kTagSize) == 0) {
+      u16 key_len;
+      memcpy(&(key_len), buf + kTagSize, kLStringLenSize);
+      fseek(fptr, key_len, SEEK_CUR);
+      LString *value = malloc(sizeof(LString *));
+      fread(&value->len, 1, kLStringLenSize, fptr);
+      value->data = malloc(value->len);
+      fread(value->data, 1, value->len, fptr);
+      return value;
+    }
+  }
+  return NULL;
+}
 
 #include <ctype.h>
 
@@ -234,7 +357,6 @@ void debug_dump_buffer(const char *label, const void *data, size_t size) {
   }
   printf("--------------------------------\n");
 }
-
 
 // int main() {
 //   srand(time(0));
@@ -278,7 +400,8 @@ void debug_dump_buffer(const char *label, const void *data, size_t size) {
 //   printf("key_a=%p\n", key_a);
 //
 //   printf("Total elements returned: %u\n", count);
-//   printf("%-5s | %-10s | %-20s | %s\n", "Idx", "Len", "Hex Content", "String");
+//   printf("%-5s | %-10s | %-20s | %s\n", "Idx", "Len", "Hex Content",
+//   "String");
 //   printf("---------------------------------------------------------------\n");
 //
 //   for (u32 i = 0; i < count; i++) {
@@ -293,8 +416,8 @@ void debug_dump_buffer(const char *label, const void *data, size_t size) {
 //     // %.*s takes the length (debug_keys[i].len) as the first arg
 //     // and the char pointer as the second.
 //     printf("Len: %-4u | Data: \"%.*s\" | Value: %.*s\n", debug_keys[i]->len,
-//            (int)debug_keys[i]->len, debug_keys[i]->data, debug_values[i]->len,
-//            debug_values[i]->data);
+//            (int)debug_keys[i]->len, debug_keys[i]->data,
+//            debug_values[i]->len, debug_values[i]->data);
 //   }
 //
 //   // Clean up the arrays allocated by sl_s_get_data
