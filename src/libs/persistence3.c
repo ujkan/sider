785ab73e99d78c9fbcbcb1621d14a34e8f2a2bd2
Ujkan Sulejmani <ujkan99@gmail.com>
2026-05-23 23:27:22 +0200

    cleanup + reimpl compress_and_Write_V2

--- src/libs/persistence3.c
+++ src/libs/persistence3.c
@@ -35,8 +35,8 @@
  */
 
 #include "block.h"
-#include "block_item.h"
 #include "bytering.h"
+#include "disk_block.h"
 #include "hex_dump.h"
 #include "index_block.h"
 #include "lstr.h"
@@ -193,140 +193,73 @@ void compress_and_write_v2(Array *sst_pairs, int data_len, char *write_buf,
                            int *len) {
 
   struct SSTPair curr = array_index(sst_pairs, struct SSTPair, 0);
-  struct DataSection *d_section = malloc(sizeof(struct DataSection));
-  arrsetcap(d_section->blocks, 128);
-  struct DataBlock new_block = {0};
-  DataBlock_init(&new_block);
-  // NOTE: new_block points to BlockItem arr
-  // that block must live beyond what happens to new_block, which
-  // is a transient variable. since we store the block BY copy in
-  // block->blocks, it means those pointers get copied, and thus a handle
-  // to them exists via block->blocks[i]
-  arrpush(d_section->blocks, new_block);
+  struct DataSection d_section = {0};
+  arrsetcap(d_section.blocks, 128);
+  struct DataBlock *curr_block = DataSection_add_new_block(&d_section);
 
-  DataBlock_append_entry(&new_block, &curr.key, &curr.value,
-                               NULL);
-  // LString *b_item_serialized = BlockItem_serialize(&b_item);
-  // memcpy(cursor, b_item_serialized->data, b_item_serialized->len);
-  // cursor += b_item_serialized->len;
-  // // free after copy
-  // lstring_free(b_item_serialized);
-
-  // TODO: for nicer programming experience, it is probably better in code to
-  // convert between representations, i.e. convert SSTPair to some new struct
-  // like BlockEntry and then rely on special methods to serialize it
-  // that way you write the code once, test it, make sure it works right
-  // and rely on simpler code to map from SSTPair to BlockEntry!
-  // these are just data transformations, mappings! should be much simpler to
-  // code
-  LString *prev = &curr.key;
+  DataBlock_append_entry(curr_block, &curr.key, &curr.value, NULL);
+  LString *prev_key = &curr.key;
   // TODO: should we copy curr.key here or just "borrow" it or get its ref?
   struct IndexItem curr_index_item = {.key = &curr.key, .offset = 0};
   struct IndexSection i_section = {0};
   arrsetcap(i_section.items, 128);
   arrpush(i_section.items, curr_index_item);
+
+  u8 *cursor = (u8 *)write_buf;
   for (u32 j = 1; j < sst_pairs->len; j++) {
-    // curr = array_index(sst_pairs, struct SSTPair, j);
-    // printf("CURR->KEY: %.*s\n", curr.key.len, curr.key.data);
-    if (j % 128 == 0) {
-      // it is  crucial that _init sets the .items and .restart_points fields
-      // to NULL and thus arrsetcap CREATES new arrays (mallocs them)
-      // recall that the reference to the old block's items and rps remains
-      // when we append to section->blocks BY VALUE ! so the pointers are copied
-      // and thus live, but curr_block gets refreshed!
-      // essentially what we want each time is something like
-      // curr_block_ = new DataBlock();
-      // where the constructor of that allocates the arrays (NEW ones)
-      // appropriately
-      // i.e. think of DataBlock_init as
-      // public void DataBlock() {
-      //   this.items = new ArrayList<BlockItem>();
-      //   this.restartPoints = new ArrayList<RestartPoint>();
-      // }
-      // curr_block_ = new DataBlock();
-      // block.blocks.append(curr_block_);
-      // ...
-      // curr_block_.items.append(b_item);
-      // ---
-      // since curr_block_ is an Object, we can append to it and
-      // block.blocks.append retains it
-      // of course oen can argue that we should append once we're done
-      // attending for more "idiomatic" usage!
-      // maybe first append, and then initialize the new current block
-      // that would work if we do the following:
-      // (1) swap the order of _init and arrpush below
-      // (2) remove the arrpush before the loop
-      // (3) add a final arrpush AFTER the loop to cover the final block
-      // it depends on if we want:
-      // 1. push block before and then fill it with data
-      // 2. fill it with data then push
-      DataBlock_init(&new_block);
-      arrpush(d_section->blocks, new_block);
-    }
     curr = array_index(sst_pairs, struct SSTPair, j);
-    prev = &array_index(sst_pairs, struct SSTPair, j - 1).key;
-    struct DataBlock *curr_block = &d_section->blocks[arrlen(d_section->blocks) - 1];
-    DataBlock_append_entry(curr_block, &curr.key, &curr.value,
-                                 prev);
+    prev_key = &array_index(sst_pairs, struct SSTPair, j - 1).key;
     if (j % 128 == 0) {
+      DataBlock_compressed_serialize_into(curr_block, &cursor);
+      curr_block = DataSection_add_new_block(&d_section);
       curr_index_item.key = &curr.key;
+      curr_index_item.offset = cursor - (u8 *)write_buf;
       arrpush(i_section.items, curr_index_item);
     }
-    // b_item_serialized = BlockItem_serialize(&b_item);
-    // // TODO: yes not ideal that we first memcpy into an LString and
-    // // then memcpy into cursor
-    // // would be nice to have a serialize into a char* directly
-    // memcpy(cursor, b_item_serialized->data, b_item_serialized->len);
-    // cursor += b_item_serialized->len;
-    // // free after copy
-    // lstring_free(b_item_serialized);
+    DataBlock_append_entry(curr_block, &curr.key, &curr.value, prev_key);
   }
-
-  LString *compressed_block = DataSection_compress(d_section);
+  DataBlock_compressed_serialize_into(curr_block, &cursor);
   LString *index_block_serialized = IndexSection_serialize(&i_section);
-  // TODO: use slice/bytebuf instead of write_buf
-  printf("compressed->block->len: %d\n", compressed_block->len);
-  u8 *cursor = (u8 *)write_buf;
-  scribe_put_bytes(&cursor, compressed_block->data, compressed_block->len);
-  *len += compressed_block->len;
   scribe_put_bytes(&cursor, index_block_serialized->data,
                    index_block_serialized->len);
-  *len += index_block_serialized->len;
-  array_set_length(sst_pairs, 0);
+  *len = (cursor - (u8 *)write_buf);
+  DataSection_destroy(&d_section);
+  lstring_free(index_block_serialized);
+  arrfree(i_section.items);
 }
 
 SSTable *dump_memtable_to_sst_v2(SkipList *mt, LString *filepath) {
-  pthread_t thread_id = pthread_self();
+  SSTable *sst = NULL;
+  SSTable *result = NULL;
+  LString **keys = NULL;
+  LString **values = NULL;
+  Array *sst_pairs = NULL;
+  int fd = -1;
+  char *map = MAP_FAILED;
+  size_t size = 1024 * 1024 * 128;
 
-  SSTable *sst = malloc(sizeof(SSTable));
+  sst = malloc(sizeof(SSTable));
   sst->filepath = filepath;
 
-  char *fp __attribute__((__cleanup__(cleanup_char_buf)));
-  fp = lstring_to_cstr(filepath); // 0-terminated means can copy 127 chars max
-  int fd;
-  fd = open(fp, O_RDWR | O_CREAT | O_TRUNC, 0644);
-  size_t size = 1024 * 1024 * 128;
-  if (ftruncate(fd, size) == -1) {
-    perror("Error setting file size");
-    return NULL;
-  }
-
-  char *map = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-
-  LString **keys;
-  LString **values;
   u32 out_count;
   sl_s_get_data(mt, &keys, &values, &out_count);
-  if (out_count == 0) {
-    free(sst);
-    free(keys);
-    free(values);
-    return 0;
-  }
+  if (out_count == 0)
+    goto cleanup;
+
+  char *fp;
+  fp = lstring_to_cstr(filepath); // 0-terminated means can copy 127 chars max
+  fd = open(fp, O_RDWR | O_CREAT | O_TRUNC, 0644);
+  if (ftruncate(fd, size) == -1)
+    goto cleanup;
+
+  map = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (map == MAP_FAILED)
+    goto cleanup;
+
   // sst_pairs_init(keys, values);
   //
-  Array *sst_pairs = array_sized_new(out_count, sizeof(struct SSTPair));
-  for (int i = 0; i < out_count; i++) {
+  sst_pairs = array_new_full(out_count, sizeof(struct SSTPair), SSTPair_free);
+  for (u32 i = 0; i < out_count; i++) {
     struct SSTPair sst_pair = {0};
     sst_pair.tag = kPairTag;
     sst_pair.key = *keys[i];
@@ -337,134 +270,29 @@ SSTable *dump_memtable_to_sst_v2(SkipList *mt, LString *filepath) {
   compress_and_write_v2(sst_pairs, 0, map, &written_len);
   if (msync(map, size, MS_SYNC) == -1) {
     perror("Could not sync to disk");
-  }
-  if (munmap(map, size) == -1) {
-    perror("Error unmapping");
+    goto cleanup;
   }
   if (ftruncate(fd, written_len) == -1) {
     perror("ftruncate failed");
+    goto cleanup;
   }
-  close(fd);
-  sst->fd = fd;
-  return sst;
+  sst->fd = -1;
+  result = sst;
+  sst = NULL;
+cleanup:
+  free(sst);
+  free((void *)keys);
+  free((void *)values);
+
+  if (sst_pairs)
+    array_free(sst_pairs, true);
+  if (map != MAP_FAILED)
+    munmap(map, size);
+  if (fd != -1)
+    close(fd);
+  return result;
 }
 
-// SSTable *dump_memtable_to_sst(SkipList *mt, LString *filepath) {
-//   pthread_t thread_id = pthread_self();
-//
-//   SSTable *sst = malloc(sizeof(SSTable));
-//   sst->filepath = filepath;
-//
-//   char *fp __attribute__((__cleanup__(cleanup_char_buf)));
-//   fp = lstring_to_cstr(filepath); // 0-terminated means can copy 127 chars
-//   max FILE *fptr __attribute__((__cleanup__(cleanup_file))); fptr = fopen(fp,
-//   "a");
-//
-//   if (fptr == NULL) {
-//     return NULL;
-//   }
-//   sst->fd = fileno(fptr);
-//
-//   LString **keys;
-//   LString **values;
-//   u32 out_count;
-//   sl_s_get_data(mt, &keys, &values, &out_count);
-//
-//   if (out_count == 0) {
-//     free(sst);
-//     free(keys);
-//     free(values);
-//     return 0;
-//   }
-//
-//   // Record the starting position to calculate total bytes written at the
-//   // end
-//   long start_pos = ftell(fptr);
-//
-//   int idx_struct_size = sizeof(struct KeyOffsetPair);
-//   Array *offset_sparse_index = array_sized_new(out_count, idx_struct_size);
-//
-//   u32 index_size = 0;
-//   u32 index_offset = 0;
-//   u32 kFooterSize = 4 + 4; // 4 bytes for size; 4 for offset
-//   int data_len = 0;
-//
-//   Array *data_blocks = array_sized_new(
-//       kBatchSize,
-//       sizeof(struct SSTPair)); // k_max_key_len + k_max_value_len (upper
-//       bound
-//                                // for block)
-//   // --- Write the data and build the index ---
-//   for (u32 i = 0; i < out_count; i++) {
-//
-//     if (i > 0 && i % kBatchSize == 0) {
-//       compress_and_write(data_blocks, data_len, fptr);
-//       data_len = 0;
-//     }
-//     if (i % kBatchSize == 0) {
-//       u32 current_offset = (u32)(ftell(fptr) - start_pos);
-//       array_push(
-//           offset_sparse_index,
-//           &(struct KeyOffsetPair){.key = *keys[i], .offset =
-//           current_offset});
-//     }
-//     LString *key = keys[i];
-//     LString *value = values[i];
-//     struct SSTPair tmpdata = {0};
-//     tmpdata.tag = kPairTag;
-//     tmpdata.key = *key;
-//     tmpdata.value = *value;
-//     array_push(data_blocks, &tmpdata);
-//     data_len += SSTPair_get_size(&tmpdata);
-//     //     printf("DATA_LEN: %d\n", data_len);
-//   }
-//   compress_and_write(data_blocks, data_len, fptr);
-//   free(data_blocks->data);
-//   free(data_blocks);
-//
-//   index_offset = ftell(fptr);
-//
-//   // --- Write the index ---
-//   u32 index_count = (out_count - 1) / kBatchSize + 1;
-//   printf("Writing index...\n");
-//   for (u32 i = 0; i < index_count; i++) {
-//     struct KeyOffsetPair *pair =
-//         &array_index(offset_sparse_index, struct KeyOffsetPair, i);
-//
-//     printf("index[%d]: key len=%d data=%.*s offset=%d\n", i, pair->key.len,
-//            pair->key.len, pair->key.data, pair->offset);
-//     index_size += KeyOffsetPair_serialize(pair, fptr);
-//   }
-//
-//   // write footer
-//   //   printf("index_offset: %d\n", index_offset);
-//   //   printf("index_size: %d\n", index_size);
-//   fwrite(&index_offset, sizeof(index_offset), 1, fptr);
-//   fwrite(&index_size, sizeof(index_size), 1, fptr);
-//
-//   // Return total bytes written
-//   sst->size = (u32)(ftell(fptr) - start_pos);
-//   // printf("[THREAD %lu] dump_memtable_to_sst: Successfully wrote %u "
-//   //        "bytes to '%s', closing file\n",
-//   //        thread_id, sst->size, fp);
-//   // printf("[THREAD %lu] dump_memtable_to_sst: Returning SST with "
-//   //        "filepath = '%s'\n ",
-//   //        thread_id, sst->filepath->data);
-//   array_set_length(offset_sparse_index, 0);
-//   free(offset_sparse_index->data);
-//   free(offset_sparse_index);
-//   // for (u32 i = 0; i < out_count; i++) {
-//   //   free(keys[i]->data);
-//   //   free(values[i]->data);
-//   // }
-//   free(keys);
-//   free(values);
-//   return sst;
-// }
-
-// tombstone will be
-// [key] [len] [value]
-// "key" 0     """
 size_t serialize_skip_list(SkipList *mt, char *buf) {
   LString **keys;
   LString **values;
@@ -942,85 +770,3 @@ void merge_and_compact_level_zero(Array *sstables) {
 //   canada, cv
 //   cyprus, cv
 
-// int main() {
-//   srand(time(0));
-//
-//   // 1. Setup SkipList
-//   SkipList *sl = malloc(sizeof(SkipList));
-//   Node *nmin = malloc(sizeof(Node) + 3 * sizeof(Node *));
-//   nmin->key = lstring_create(0);
-//   nmin->level = 2;
-//   for (int i = 0; i < 3; i++)
-//     nmin->next[i] = NULL;
-//   sl->head = nmin;
-//   sl->num_levels = 3;
-//
-//   // 2. Insert Test Data (using distinct values to make them easy to find
-//   in
-//   // hex)
-//   printf("Populating Memtable...\n");
-//   LString *key_a = lstring_create_from_buf(5, "KEYA1");
-//   LString *value_a = lstring_create_from_buf(7, "value_1");
-//   LString *value_b = lstring_create_from_buf(7, "value_Z");
-//   sl_s_insert(sl, key_a, value_a);
-//   sl_s_insert(sl, lstring_create_from_buf(5, "KEYZx"), value_b);
-//
-//   // 3. Serialize
-//   char buffer[2048];
-//   memset(buffer, 0, 2048);
-//
-//   // --- After inserting data into 'sl' ---
-//
-//   printf("\n=== Debugging sl_s_get_data Output ===\n");
-//
-//   LString **debug_keys;
-//   LString **debug_values;
-//   u32 count;
-//
-//   // Call the function we want to inspect
-//   sl_s_get_data(sl, &debug_keys, &debug_values, &count);
-//   printf("debug_keys[0]=%p\n", debug_keys[0]->data);
-//   printf("debug_keys[0].len=%d\n", debug_keys[0]->len);
-//   printf("debug_keys[1]=%p\n", debug_keys[1]->data);
-//   printf("key_a=%p\n", key_a);
-//
-//   printf("Total elements returned: %u\n", count);
-//   printf("%-5s | %-10s | %-20s | %s\n", "Idx", "Len", "Hex Content",
-//   "String");
-//   printf("---------------------------------------------------------------\n");
-//
-//   for (u32 i = 0; i < count; i++) {
-//     printf("[%u] ", i);
-//
-//     // Check for NULL data to prevent segfaults
-//     if (debug_keys[i]->data == NULL) {
-//       printf("DATA IS NULL\n");
-//       continue;
-//     }
-//
-//     // %.*s takes the length (debug_keys[i].len) as the first arg
-//     // and the char pointer as the second.
-//     printf("Len: %-4u | Data: \"%.*s\" | Value: %.*s\n",
-//     debug_keys[i]->len,
-//            (int)debug_keys[i]->len, debug_keys[i]->data,
-//            debug_values[i]->len, debug_values[i]->data);
-//   }
-//
-//   // Clean up the arrays allocated by sl_s_get_data
-//   free(debug_keys);
-//   free(debug_values);
-//
-//   printf("======================================\n\n");
-//   size_t written = serialize_skip_list(sl, buffer);
-//
-//   // 4. Debug Output
-//   if (written > 0) {
-//     debug_dump_buffer("SSTable Sparse Index Serialized Data", buffer,
-//                       (size_t)written);
-//   } else {
-//     printf("No data written (check if batch_size is skipping your small "
-//            "dataset)\n");
-//   }
-//
-//   return 0;
-// }
